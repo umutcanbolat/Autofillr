48d29ac51d140c07d4e37b6ad8fc923d4ddaa20e
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2020-10-25 01:47:09 +0200

    feat: 🎸 run contentScript on all iframes in the page

--- src/contentScripts/content.js
+++ src/contentScripts/content.js
@@ -1,4 +1,5 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log(sender.tab ? `from a content script: ${sender.tab.url}` : 'from the extension');
+  console.log(document.getElementById('email'));
   if (request.greeting === 'hello') sendResponse({ farewell: 'goodbye' });
 });
