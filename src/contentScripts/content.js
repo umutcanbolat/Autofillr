6ffd5123eaec40eeb37fb323f9857430d1b50bd7
umutcanbolat <hello@umutcanbolat.com>
2021-09-08 21:58:11 +0200

    fix: 🐛 clear the filled inputs before filling again

--- src/contentScripts/content.js
+++ src/contentScripts/content.js
@@ -1,3 +1,16 @@
+let filledInputs = new Set();
+
+const clearFilledInputs = () => {
+  filledInputs.forEach((target) => {
+    target.dispatchEvent(new Event('focus', { bubbles: true }));
+    // eslint-disable-next-line no-param-reassign
+    target.value = '';
+    target.dispatchEvent(new Event('input', { bubbles: true }));
+  });
+
+  filledInputs = new Set();
+};
+
 const setInput = (target, value) => {
   target.dispatchEvent(new Event('focus', { bubbles: true }));
   // eslint-disable-next-line no-param-reassign
@@ -7,21 +20,22 @@ const setInput = (target, value) => {
 };
 
 const fillAvailableFields = (request, _sender, sendResponse) => {
+  // Clear the previously filled inputs. This is required because some forms remove the autocomplete attribute, after input value is modified.
+  // This behaviour is there to prevent browsers from overriding the manually put inputs.
+  clearFilledInputs();
+
   Object.values(request).forEach(({ value, autocomplete }) => {
     try {
       const target = document.querySelectorAll(`input[autocomplete*='${autocomplete}']`)[0];
-
-      // clear the input
-      setInput(target, '');
-
       // set the input
       setInput(target, value);
+      filledInputs.add(target);
     } catch (e) {
       // let's pretend like nothing has happened
     }
   });
 
-  sendResponse('the form is filled');
+  sendResponse('Autofillr successfully filled the form.');
 };
 
 chrome.runtime.onMessage.addListener(fillAvailableFields);
