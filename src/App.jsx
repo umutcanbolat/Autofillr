d2864a26c936d5d33ac59c0106ad6ffa941bf1e9
umutcanbolat <hello@umutcanbolat.com>
2024-04-18 23:35:19 +0200

    refactor: 💡 handle unhandled error on sendmessage

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,16 @@ function App() {
 
   const fill = useCallback(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, formData, (response) => {
-        // eslint-disable-next-line no-console
-        console.log(response);
-      });
+      chrome.tabs
+        .sendMessage(tabs[0].id, formData)
+        .then((response) => {
+          // eslint-disable-next-line no-console
+          console.log(response);
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.error(error);
+        });
     });
   }, [formData]);
 
